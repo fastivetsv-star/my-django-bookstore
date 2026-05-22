b2029d7064acf74b3fb1f0dd0090de6d3c66036b
fastivetsv-star <fastivetsv@gmail.com>
2026-05-22 16:48:38 +0200

    Fix entrypoint to run Gunicorn on port 10000

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-echo "⏳ Виконуємо автоматичні міграції бази даних (PostgreSQL)..."
-python manage.py makemigrations
+echo "⏳ Виконуємо міграції бази даних (PostgreSQL)..."
 python manage.py migrate
 
 echo "✅ Міграції успішно застосовано!"
 
-echo "🚀 Запускаємо Django..."
-exec "$@"
+echo "🚀 Запускаємо Django через Gunicorn..."
+exec gunicorn mysite.wsgi:application --bind 0.0.0.0:10000
